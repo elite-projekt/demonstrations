4d903068e148b2d09ab7071496107b44b562d4bf
Marco Warnke <marco.warnke@stud.h-da.de>
2021-01-26 18:59:27 +0100

    ci: build navigation image

---
+++ ci/push-navigation-image.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "starting ci script"
+
+mkdir -p /kaniko/.docker
+echo "{\"auths\":{\"$CI_REGISTRY\":{\"username\":\"$CI_REGISTRY_USER\",\"password\":\"$CI_REGISTRY_PASSWORD\"}}}" > /kaniko/.docker/config.json
+
+cd teachingMaterialFront
+image_name="$CI_REGISTRY_IMAGE/navigation:$CI_COMMIT_BRANCH"
+echo "pushing to $image_name"
+
+/kaniko/executor --context "$CI_PROJECT_DIR/Demo Navigation/" --dockerfile "$CI_PROJECT_DIR/Demo Navigation/Dockerfile" --destination "$image_name"
