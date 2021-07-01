c2be1a91eb64909fa13f6b09a56d090a63b7b49b
stptmue2 <stptmue2@stud.h-da.de>
2021-07-01 17:35:45 +0200

    added cli arguments for .exe file
    Arguments via cli now passable, it's possible to pass a path starting the exe somewhere and passing the directory where all dependencies are placed now possible
    
    Relates: #37

--- native/native/app.py
+++ native/native/app.py
@@ -4,6 +4,7 @@ from config.config import DevelopmentConfig, ProductionConfig
 from controller.OrchestrationController import orchestration
 import sys
 import logging
+import argparse
 
 app = Flask(__name__)
 CORS(app)
@@ -14,19 +15,22 @@ if __name__ == "__main__":
     # set working directory
     app.WORKINGDIR = os.getenv('ProgramFiles(x86)') + r'\hda\nativeapp'
 
+    # argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help='Basepath of project relative from here files will be accessed. Default is "C:\Program Files (x86)\hda\nativeapp".')
+    parser.add_argument('-d', '--dev', help='Starts flask in dev-mode')
+    args = parser.parse_args()
+
+    # Only for debugging while developing
+    if args.dev is not None:
+        app.config.from_object(DevelopmentConfig)
+    else:
+        app.config.from_object(ProductionConfig)
+    if args.path is not None:
+        app.WORKINGDIR = args.path
+
     # initiate logging
     logging.basicConfig(filename=app.WORKINGDIR + '\\service.log', datefmt='%y-%m-%d %H:%M:%S', format='%(asctime)s %(levelname)-8s - [%(module)s:%(funcName)s] : %(message)s', level=logging.DEBUG)
     logging.info('Starting service: native app')
-
-    # Only for debugging while developing
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "prod":
-            app.config.from_object(ProductionConfig)
-        elif sys.argv[1] == "dev":
-            app.config.from_object(DevelopmentConfig)
-        elif sys.argv[1] == "path":
-            app.WORKINGDIR = str(sys.argv[2])
-    else:
-        app.config.from_object(DevelopmentConfig)
 
     app.run(host=app.config["HOST"], debug=app.config["DEBUG"], port=app.config["PORT"])
