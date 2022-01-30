33715c6285ecd396c958f17aee80094dbce273b7
Nikolai Wiegand <nikolai.wiegand@stud.h-da.de>
2022-01-30 21:04:15 +0100

    added frontend to current backend branch

---
+++ demos/download/src/frontend/src/assets/constant.js
@@ -0,0 +1,16 @@
+export const MULTISCAN = 'props.multiScan'
+
+export const LABELS = [
+    {
+        type: 'discount',
+        class: 'discount'
+    },
+    {
+        type: 'advertisement',
+        class: 'advertisement'
+    },
+    {
+        type: 'new',
+        class: 'new'
+    }
+]
