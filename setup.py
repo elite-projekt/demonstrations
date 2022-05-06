ce2594f85631bab3027c8072d3b160146f7d8b2b
Kevin Köster <kevin.koester@uni-hamburg.de>
2022-05-06 11:01:01 +0200

    Not good but way better

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import tempfile
 import pathlib
-import tarfile
 import sys
 
 from subprocess import Popen, PIPE  # nosec
@@ -8,63 +6,23 @@ from setuptools import setup, find_packages
 
 from typing import List
 
-
-WSL_PATCH = """
---- a/python_on_whales/client_config.py
-+++ b/python_on_whales/client_config.py
-@@ -85,7 +85,7 @@ class ClientConfig:
-     @property
-     def docker_cmd(self) -> Command:
-
--        result = Command([self.get_docker_path()])
-+        result = Command(["wsl", "--user", "root", "docker"])
-
-         if self.config is not None:
-             result += ["--config", self.config]
-"""
-
 PYTHON_EXE = sys.executable
+PIP_URL = r"git+https://gitlab.com/kevin.koester/python_on_whales"
 
 
-def call_cmd(command_list: List[str], workdir: pathlib.Path):
+def call_cmd(command_list: List[str], workdir: pathlib.Path = None):
+    print(command_list)
     with Popen(  # nosec
             command_list, stdout=PIPE, cwd=workdir) as process:
         process.communicate()
 
 
 def install_patched_pow():
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        tmpdir = pathlib.Path(tmpdirname)
-        # Download python on whales
-        call_cmd([f"{PYTHON_EXE}",
-                  "-m", "pip", "download",
-                  "--no-deps", "--no-binary", ":all:", "python-on-whales"],
-                 tmpdir)
-        call_cmd([f"{PYTHON_EXE}", "-m", "pip", "install", "patch"], tmpdir)
-        tar_files = list(tmpdir.glob("*.tar.gz"))
-        if len(tar_files) != 1:
-            print("ERROR while downloading dependencies")
-            sys.exit(1)
-        tar_file = tar_files[0]
-
-        with tarfile.open(tar_file) as tar:
-            tar.extractall(path=tmpdir)
-        pow_folder = tmpdir / pathlib.Path(tar_file.stem).stem
-
-        if not pow_folder.exists():
-            print("ERROR while extracting python on whales")
-            sys.exit(1)
-
-        with open(pow_folder / "wsl.patch", "w") as patch_file:
-            patch_file.write(WSL_PATCH)
-        call_cmd([f"{PYTHON_EXE}", "-m", "patch", "wsl.patch"], pow_folder)
-        call_cmd([f"{PYTHON_EXE}", "-m", "pip", "install", "-I", "."],
-                 pow_folder)
+    call_cmd([f"{PYTHON_EXE}", "-m", "pip", "install", PIP_URL])
 
 
 if sys.platform == "win32":
     install_patched_pow()
-
 
 setup(
     packages=find_packages(where="native") +
