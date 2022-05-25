2ec95b727228f998e4053dbcde29f7e80717c211
Budagow <mohamed.budagow@fokus.fraunhofer.de>
2022-05-25 15:51:22 +0200

    added dns name to the installer

--- native/installer/nativeapp_install_helper.ps1
+++ native/installer/nativeapp_install_helper.ps1
@@ -181,7 +181,7 @@ If(!(Test-Path -path $rootPath)) {
     }
 
     # Host File
-    $localRedirects = "127.0.0.1`twww.shipment-support-amazon.com covidsupportgermany.de coronahilfengermany.de mpseinternational.com mail.domain.com printer.io #MPSE"
+    $localRedirects = "127.0.0.1`twww.shipment-support-amazon.com covidsupportgermany.de coronahilfengermany.de mpseinternational.com mail.domain.com printer.io de.linkedln.com #MPSE"
     try {
         $containsRedirecets = Select-String -Path $hostFile -Pattern '#MPSE' -ErrorAction Stop
         if($containsRedirecets -ne $null) {
