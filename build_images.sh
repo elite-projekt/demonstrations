829e468f9bd4a83cc452e7969fe06899d195b620
Kevin Köster <kevin.koester@uni-hamburg.de>
2022-08-26 15:37:35 +0200

    Fix build images script

--- build_images.sh
+++ build_images.sh
@@ -6,16 +6,16 @@ function build_demo() {
   DOCKER_PATH=$1
   TAG=$2
 
-  ${SCRIPT_DIR}/build_image.sh -c ${DOCKER_PATH} -i ${TAG} -u -p
+  "${SCRIPT_DIR}/build_image.sh" -c ${DOCKER_PATH} -i ${TAG} -u -p
 }
 
-build_demo ${SCRIPT_DIR}/demos/uhh_ducky_mitm/container/mitmproxy uhh_ducky_mitm_proxy
-build_demo ${SCRIPT_DIR}/demos/uhh_ducky_mitm/container/nginx uhh_ducky_mitm_web
+build_demo "${SCRIPT_DIR}/demos/uhh_ducky_mitm/container/mitmproxy" uhh_ducky_mitm_proxy
+build_demo "${SCRIPT_DIR}/demos/uhh_ducky_mitm/container/nginx" uhh_ducky_mitm_web
 
-build_demo ${SCRIPT_DIR}/demos/fokusrnware fokusrnware-image
+build_demo "${SCRIPT_DIR}/demos/fokusrnware" fokusrnware
 
-build_demo ${SCRIPT_DIR}/demos/ransomware ransomware-image
+build_demo "${SCRIPT_DIR}/demos/ransomware" ransomware
 
-build_demo ${SCRIPT_DIR}/demos/phishing phishing-image
+build_demo "${SCRIPT_DIR}/demos/phishing" phishing
 
-build_demo ${SCRIPT_DIR}/demos/password password-image
+build_demo "${SCRIPT_DIR}/demos/password" password
